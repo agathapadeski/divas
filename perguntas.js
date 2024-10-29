d470e04589cc5503b9700f19496b824de9775a73
agathapadeski <agatha.padeski@escola.pr.gov.br>
2024-10-29 07:47:26 -0300

    fghn

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criacartao(
+    'Programação',
+    'O que é um algoritimo?',
+    'É uma sequência de instruções passo a passo para resolver um problema.')
+criacartao(
+    'Programação',
+    'Qual a diferença entre um compilador e um interpretador?',
+    'Compilador traduz todo o código-fonte de uma vez para um código executável, enquanto um interpretador traduz e executa o código linha por linha.')
+ criacartao(
+    'Programação', 
+    'O que é uma variável?',
+    'Um espaõ na memória onde armazenamos dados')
+    criacartao(
+        'Programação',
+        'O que significa OOP?',
+        'Significa Programação Orientada a Objetos')
+    criacartao(
+        'P'
+    )
